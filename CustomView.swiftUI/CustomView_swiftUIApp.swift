64eb9db4a90363fdbdf2c7335fc197c08d07cdf1
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-01-08 22:39:49 +0900

    Initial Commit

---
+++ CustomView.swiftUI/CustomView_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomView_swiftUIApp.swift
+//  CustomView.swiftUI
+//
+//  Created by 이윤지 on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomView_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
