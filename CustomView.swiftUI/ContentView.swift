6ee8789e9c97e1b86363ced24c1cebee438c7bc9
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-01-12 21:53:04 +0900

    포커스뷰 복습

--- CustomView.swiftUI/ContentView.swift
+++ CustomView.swiftUI/ContentView.swift
@@ -42,10 +42,11 @@ struct ContentView: View {
     }
 }
 
+
 struct FocusView: View {
     @State private var isFocused = false
     
-    var body: some View {
+    var body : some View {
         Rectangle()
             .foregroundColor(Color.green)
             .focusable(true) { focused in
@@ -54,11 +55,11 @@ struct FocusView: View {
             .frame(width: 400, height: 200)
             .scaleEffect(isFocused ? 1.2 : 1.0)
             .animation(.easeInOut(duration: 0.1))
+        
     }
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+#Preview{
+    ContentView()
 }
